71319fff81763f69dd63d5cb6d8813d05e14f3e3
Rajkumar Rickysana Singh <rkrickysana12@gmail.com>
2026-02-17 23:13:26 +0530

    hero name

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
 
-        <h1 className='text-4xl md:text-5xl font-semibold'> Cars on Rent</h1>
+        <h1 className='text-4xl md:text-5xl font-semibold'> Vehical on Rent</h1>
 
         <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgbs(0,0,0,0.1)]'>
 
